12073942c6de260c24e445094c8da1e646d307e7
Lucas Ferreira dos Santos <fs.lucas22@gmail.com>
2023-01-22 19:08:36 -0300

    Movido o try-with-resources para o método main da classe Principal

--- src/main/java/Principal.java
+++ src/main/java/Principal.java
@@ -1,16 +1,26 @@
+import model.Conector;
+import model.Modelo;
+import model.entities.*;
+
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Principal {
+    private static final DateTimeFormatter formatHora = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     public static void main(String ... args) {
         String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:XE";
         String nomeUsuario = "testuser";
         String senha = "12345";
-        try {
-            DBConnection dbconnection = DBConnection.connect(
-                    jdbcUrl,
-                    nomeUsuario,
-                    senha
-            );
+        try (Connection conexao = Conector.conectar(
+                jdbcUrl,
+                nomeUsuario,
+                senha
+        )){
+            var modelo = new Modelo(conexao);
+            var pessoa = modelo.criaPessoa("Teste", LocalDate.parse("22/06/1999", formatHora));
+            System.out.println(pessoa);
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Um erro ocorreu: ");
             System.out.println(e.getMessage());
--- src/main/java/model/Conector.java
+++ src/main/java/model/Conector.java
@@ -6,9 +6,8 @@ public class Conector {
     public static Connection conectar(String url, String username, String password)
             throws ClassNotFoundException, SQLException {
         Class.forName("oracle.jdbc.driver.OracleDriver");
-        try(Connection conexao = DriverManager.getConnection(url, username, password)) {
-            conexao.setAutoCommit(true);
-            return conexao;
-        }
+        Connection conexao = DriverManager.getConnection(url, username, password);
+        conexao.setAutoCommit(true);
+        return conexao;
     }
 }
