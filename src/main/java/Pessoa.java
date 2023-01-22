3659479cefbe2d8342542e4806973b0de13816f7
Lucas Ferreira dos Santos <fs.lucas22@gmail.com>
2023-01-22 13:17:42 -0300

    Criada classe Pessoa para representar uma pessoa no banco de dados

--- src/main/java/Pessoa.java
+++ src/main/java/Pessoa.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Pessoa {
+import java.time.LocalDate;
+
+public class Pessoa {
+    private String nome;
+    private LocalDate dataNascimento;
+
+    public Pessoa(String nome, LocalDate dataNascimento) {
+        this.nome = nome;
+        this.dataNascimento = dataNascimento;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
 }
