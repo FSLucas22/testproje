1385f4304219de9196455bf672560dad34660d18
Lucas Ferreira dos Santos <fs.lucas22@gmail.com>
2023-01-22 20:31:09 -0300

    Removido o método criaFuncionario, pois agora o método é exclusivo da classe ModeloFuncionario

--- src/main/java/model/ModeloPessoa.java
+++ src/main/java/model/ModeloPessoa.java
@@ -28,25 +28,6 @@ public class ModeloPessoa {
             return new Pessoa(id, nome, dataNascimento);
         }
     }
-    public Funcionario criaFuncionario(String nome, LocalDate dataNascimento, BigDecimal salario, String funcao) throws SQLException {
-        String [] toReturn = {"id"};
-        try (PreparedStatement comando = conexao.prepareStatement(
-                "INSERT INTO Funcionario(nome, data_nascimento, salario, funcao) VALUES (?, ?, ?, ?)",
-                toReturn
-        )
-        ) {
-            comando.setString(1, nome);
-            comando.setObject(2, dataNascimento);
-            comando.setObject(3, salario);
-            comando.setString(4, funcao);
-            comando.executeUpdate();
-            ResultSet campos_gerados = comando.getGeneratedKeys();
-            campos_gerados.next();
-            int id = campos_gerados.getInt(1);
-            return new Funcionario(id, nome, dataNascimento, salario, funcao);
-        }
-    }
-
     public Connection getConexao() {
         return conexao;
     }
